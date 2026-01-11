7d593cdb579efe685cf054d072699b4c7b5ae706
Mohamed Samir <mohamedbahr3836@gmail.com>
2026-01-11 13:02:03 +0200

    fix kafka producer implementation

--- src/infrastructure/kafka/producer.js
+++ src/infrastructure/kafka/producer.js
@@ -3,18 +3,22 @@ const kafka = require("./kafkaClient");
 const producer = kafka.producer();
 
 const sendLog = async (log) => {
-  await producer.connect();
+  try {
+    await producer.connect();
 
-  await producer.send({
-    topic: "user-logs",
-    messages: [
-      {
-        value: JSON.stringify(log),
-      },
-    ],
-  });
+    await producer.send({
+      topic: "user-logs",
+      messages: [
+        {
+          value: JSON.stringify(log),
+        },
+      ],
+    });
 
-  console.log("Log sent:", log);
+    console.log("Log sent to Kafka");
+  } catch (err) {
+    console.error("Error sending log to Kafka", err);
+  }
 };
 
 module.exports = { sendLog };
